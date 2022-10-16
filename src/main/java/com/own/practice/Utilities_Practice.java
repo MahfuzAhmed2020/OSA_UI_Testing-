f3b150a5557d2e911605f419a608121eb2465ced
mahfuz <20mahfuzahmed20@gmail.com>
2022-10-16 13:09:06 -0400

    changing the class name into OwnUtilities from MahfuzBranch

--- src/main/java/com/own/practice/Utilities_Practice.java
+++ src/main/java/com/own/practice/Utilities_Practice.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
-public class MahfuzBranch {
+public class Utilities_Practice {
 	static WebDriver dr;
 
 	public static void main(String[] args) {
